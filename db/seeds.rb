453defb5d86ce782f0d8e0340cdcc4efd5a06b54
Mark Richardson <mark@markeban.com>
2016-09-08 19:46:08 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+100.times do 
+  Employee.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    birthdate: Faker::Date.backward(55000),
+    ssn: Faker::Code.ean
+  )
+end 
 
 200.times do
   Address.create(
@@ -8,4 +17,5 @@
     :zip => Faker::Address.zip_code, 
     :employee_id => rand(1..100)
     )
+end
 end
