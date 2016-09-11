e4491a21ddf8ba3aa706fc0b4bebd1ba86d1e1b5
Mark Richardson <mark@markeban.com>
2016-09-11 10:03:36 -0700

    secured api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :restrict_access
   protect_from_forgery with: :null_session
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |api_key, options|
+      User.find_by(api_key: api_key, email: request.headers['X-User-Email'])
+    end
+  end
+
+
+
+
+
 end
